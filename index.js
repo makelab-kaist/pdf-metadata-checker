3152dcb22bd46974cb5c7bbc841211d430787d61
Andrea Bianchi <andrea@kaist.ac.kr>
2024-09-11 16:02:43 +0900

    Fixed bug with extensions

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ import path from 'path';
 import { log } from 'console';
 
 const dir = path.join('.', 'pdf');
-const files = fs.readdirSync(dir).map((f) => path.join(dir, f));
+const files = fs
+  .readdirSync(dir)
+  .filter((f) => f.endsWith('.pdf'))
+  .map((f) => path.join(dir, f));
 
 files.forEach(async (file) => {
   const bytes = fs.readFileSync(file);
-  const pdfDoc = await PDFDocument.load(bytes, {
-    updateMetadata: false,
-  });
 
-  const author =
-    pdfDoc.getAuthor() === undefined ? '✅' : `❌ ${pdfDoc.getAuthor()}`;
-  log(file, pdfDoc.getTitle(), '==>', author);
+  try {
+    const pdfDoc = await PDFDocument.load(bytes, {
+      updateMetadata: false,
+    });
+    const author =
+      pdfDoc.getAuthor() === undefined ? '✅' : `❌ ${pdfDoc.getAuthor()}`;
+    log(file, pdfDoc.getTitle(), '==>', author);
+  } catch (e) {
+    log('Skipping file ', file);
+  }
 });
